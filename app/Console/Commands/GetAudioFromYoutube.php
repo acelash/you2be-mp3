a7107c9d18acc7cecf8e6cb6a0136e1779ef8b54
Andrei Gudima <gudima.andrei.512@gmail.com>
2017-10-04 10:11:34 +0300

    dev

--- app/Console/Commands/GetAudioFromYoutube.php
+++ app/Console/Commands/GetAudioFromYoutube.php
@@ -25,11 +25,12 @@ class GetAudioFromYoutube extends Command
 
         foreach ($songs as $song){
             $filename  = $path.$song->source_id.".%(ext)s";
-            $command = 'youtube-dl -o "/home/admin/web/mp3.cardeon.ru/public_html/'.$filename.'"  --extract-audio --audio-format mp3 https://www.youtube.com/watch?v='.$song->source_id;
+            $command = 'youtube-dl -o "/home/admin/web/mp3.cardeon.ru/public_html/'.$filename.'"  --extract-audio  https://www.youtube.com/watch?v='.$song->source_id;
             echo "command: ".$command." \n";
-            shell_exec($command);
+            $output = shell_exec($command);
+            echo "output: ".$output." \n";
             $song->update([
-                'state_id' => config("constants.STATE_WITH_AUDIO"),
+              //  'state_id' => config("constants.STATE_WITH_AUDIO"),
                 'file_path' => asset($filename)
             ]);
         }
