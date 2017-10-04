7bb69b080e16f3421d17696f873d45cf97f7e78d
Andrei Gudima <gudima.andrei.512@gmail.com>
2017-10-04 16:51:27 +0300

    dev

--- app/Console/Commands/GetAudioFromYoutube.php
+++ app/Console/Commands/GetAudioFromYoutube.php
@@ -22,11 +22,11 @@ class GetAudioFromYoutube extends Command
         $songs = (new Song())->getAll(true)
             ->where("songs.state_id",config("constants.STATE_CHECKED"))
             ->get();
-
+        echo "songs found:".$songs->count()." \n";
         foreach ($songs as $song){
             $filename  = $path.$song->id.".%(ext)s";
            // $command = 'youtube-dl -o "/home/admin/web/mp3.cardeon.ru/public_html/'.$filename.'"  -f "bestaudio"  https://www.youtube.com/watch?v='.$song->source_id;
-            $command = 'youtube-dl -o "/home/admin/web/mp3.cardeon.ru/public_html/'.$filename.'"  --extract-audio --audio-format mp3 https://www.youtube.com/watch?v='.$song->source_id;
+            $command = 'youtube-dl -o "/home/admin/web/mp3.cardeon.ru/public_html/'.$filename.'"  --extract-audio --audio-format mp3 --audio-quality 160K https://www.youtube.com/watch?v='.$song->source_id;
             //echo "command: ".$command." \n";
             $output = shell_exec($command);
             echo "output: ".$output." \n";
