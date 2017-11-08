6874ffc8aa810a23c54e7389248566d011c92bcd
Andrei Gudima <gudima.andrei.512@gmail.com>
2017-11-08 16:04:55 +0200

    fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,14 +45,6 @@
             }
         }
     </script>
-
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-1019858116718047",
-            enable_page_level_ads: true
-        });
-    </script>
 </head>
 <body>
 <!-- Fixed navbar -->
